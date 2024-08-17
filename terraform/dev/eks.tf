f3542668baf53cd6bd57f2497c565aec20066322
Kaushal Seelam <68200659+kaushalseelam@users.noreply.github.com>
2024-08-17 16:16:58 -0700

    Update eks.tf
    - updating to new version changes

--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -3,26 +3,26 @@ module "eks" {
   version         = "20.23.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.30"
-  subnets         = module.vpc.private_subnets
+  subnet_ids         = module.vpc.private_subnets
 
   vpc_id = module.vpc.vpc_id
 
-  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
+  node_groups = {
+    eks_nodes = {
+      desired_capacity     = 1
+      max_capacity         = 2
+      min_capacity         = 1
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t3.small"
-      additional_userdata           = "echo foo bar"
+      instance_type = "t3.small"
+
+      additional_userdata = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 1
-      asg_max_size  = 2
 
     }
-  ]
+  }
 }
+
+
 
 data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_id
