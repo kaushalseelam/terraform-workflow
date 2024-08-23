7a144ee996892ce95e2cc67e1c8e860e239d4b6d
Kaushal Seelam <kaushal.seelam@gmail.com>
2024-08-22 18:32:15 -0700

    Changing region to us-west-1

--- terraform/dev/main.tf
+++ terraform/dev/main.tf
@@ -5,14 +5,14 @@ variable "allowed_account_id" {
 
 
 provider "aws" {
-  region              = "eu-north-1"
+  region              = "us-west-1"
   allowed_account_ids = [var.allowed_account_id]
 }
 
 terraform {
   backend "s3" {
-    bucket = "andrei-terraform-state-2"
-    key    = "default-infrastructure"
-    region = "eu-north-1"
+    bucket = "andrei-terraform-state-3"
+    key    = "default-infrastructure-3"
+    region = "us-west-1"
   }
 }
