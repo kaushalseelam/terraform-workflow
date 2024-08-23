56c278bebeba3cd14ca1aef4793beea9ef86bd1e
Kaushal Seelam <kaushal.seelam@gmail.com>
2024-08-23 08:59:35 -0700

    Comment out helm resources

--- terraform/dev/helm.tf
+++ terraform/dev/helm.tf
@@ -1,14 +1,14 @@
-provider "helm" {
-  kubernetes {
-    host = data.aws_eks_cluster.cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
-    }
-}
-}
+#provider "helm" {
+#  kubernetes {
+#    host = data.aws_eks_cluster.cluster.endpoint
+#    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#    exec {
+#      api_version = "client.authentication.k8s.io/v1beta1"
+#      command     = "aws"
+#      args = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+#    }
+#}
+#}
 #https://registry.terraform.io/providers/hashicorp/helm/latest/docs/resources/release
 #resource "helm_release" "nginx" {
 #  chart = "nginx"
@@ -29,19 +29,19 @@ provider "helm" {
 #    )
 #  ]
 #}
-resource "helm_release" "localchart2" {
-  name       = "my-local-chart2"
-  chart      = "./charts/chart2"
-  #https://github.com/hashicorp/terraform-provider-helm/issues/515
-  values = [
-    yamlencode({dummy: uuid()})
-  ]
-  set {
-    name  = "recreate_pods"
-    value = "true"
-  }
-  set {
-    name  = "force_update"
-    value = "true"
-  }
-}
+#resource "helm_release" "localchart2" {
+#  name       = "my-local-chart2"
+#  chart      = "./charts/chart2"
+#  #https://github.com/hashicorp/terraform-provider-helm/issues/515
+#  values = [
+#    yamlencode({dummy: uuid()})
+#  ]
+#  set {
+#    name  = "recreate_pods"
+#    value = "true"
+#  }
+#  set {
+#    name  = "force_update"
+#    value = "true"
+#  }
+#}
