7c4b1182fa3051c586d1d2d18f92a09bb9c6b556
Kaushal Seelam <kaushal.seelam@gmail.com>
2024-08-23 08:54:28 -0700

    Comment out data blocks

--- terraform/dev/eks.tf
+++ terraform/dev/eks.tf
@@ -24,10 +24,13 @@ module "eks" {
 
 
 
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_name
-}
+# Comment out the data blocks
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = module.eks.cluster_name
+# }
+
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = module.eks.cluster_name
+# }
+
